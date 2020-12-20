02e13bfe3db23faea4f0a3939287c55f8b8bbc03
Ian <DrilonIan@yahoo.com>
2020-12-20 16:04:46 +0800

    deploy backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '201c6068f1dae92be088f957658da2fb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f29cb92b-dc47-44f5-a38f-9a106ad0007d'
+};
