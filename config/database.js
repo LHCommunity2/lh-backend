c11a45ed8916a059afea561dc7ceaf1c16c14ea6
Ian <DrilonIan@yahoo.com>
2020-12-25 21:02:40 +0800

    new monggo atlas database

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         client: 'mongo',
-        uri : `mongodb://lh-backend:${process.env.PASSWORD}@cluster0-shard-00-00.22nxh.mongodb.net:27017,cluster0-shard-00-01.22nxh.mongodb.net:27017,cluster0-shard-00-02.22nxh.mongodb.net:27017/<lh-global>?ssl=true&replicaSet=atlas-a5rxh8-shard-0&authSource=admin&retryWrites=true&w=majority`
+        uri : `mongodb://lh-backend:${process.env.PASSWORD}@lhglobal-shard-00-00.fwjey.mongodb.net:27017,lhglobal-shard-00-01.fwjey.mongodb.net:27017,lhglobal-shard-00-02.fwjey.mongodb.net:27017/<lhGlobal>?ssl=true&replicaSet=atlas-h2guxr-shard-0&authSource=admin&retryWrites=true&w=majority`
       },
       options: {
         "ssl" : true,
